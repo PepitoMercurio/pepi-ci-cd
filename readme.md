fead3647148a2fed0e3f9c889bf6b5db62eb154a
Pepito Mercurio <coco.run91@gmail.com>
2025-04-15 10:31:31 +0200

    aaa

--- readme.md
+++ readme.md
@@ -1,1 +1,37 @@
 https://github.com/ynovzelab/ci-cd-ex1
+
+// intialisation project node : npm init
+// installation de TS : npm i --save-dev typescript
+// Config TS : npx tsc --init
+// installation de sass : npm i sass
+// installation de prettier : npm install --save-dev prettier
+// installation de lint : npm install eslint 
+// intialisation eslint : npx eslint --init
+
+// Lisibilité : prettier 
+// Qualité : Lint
+// Transpilation : TS / SASS
+... test unitaire : Jest
+
+// Mise en ligne sur une plateforme : Render 
+// build : transpiler, controller, vérifier (ts, lint, prettier)
+// npm run start 
+// variables d'env => à mettre dans le .gitignore
+// dist => dans le .gitignore
+// Versionner dans un repo : avec deux branches : main et development
+// Mise en ligne sur render 
+// Déclencher une mise en ligne à l'aide d'un pull request de dev => main
+
+  "scripts": {
+    "dev": "nodemon ./dist/app.js",
+    "sass:watch": "sass --watch ./src/styles:./public/css",
+    "sass:build": "sass ./src/styles:./public/css",
+    "start": "node ./dist/app.js",
+    "tsc:build": "tsc",
+    "ts:watch": "tsc --watch",
+    "prettier:fix": "prettier --write ./src/**/*.ts",
+    "prettier:check": "prettier --check ./src/**/*.ts",
+    "lint": "eslint src --ext .ts",
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "build": "npm run tsc:build && npm run sass:build && npm run prettier:check && npm run lint"
+  },
--- src/old_app.ts
+++ src/old_app.ts
@@ -1,53 +1,59 @@
 type Role = "admin" | "user" | "guest";
 
-interface Product { 
-    readonly id: number;
-    name: string;
-    price?: number
+interface Product {
+  readonly id: number;
+  name: string;
+  price?: number;
 }
 
 interface User {
-    firstName: string;
-    lastName: string;
-    age: number;
-    role: Role;
-    isActive: boolean;
-    createdAt?: Date;
-    products: Product[];
+  firstName: string;
+  lastName: string;
+  age: number;
+  role: Role;
+  isActive: boolean;
+  createdAt?: Date;
+  products: Product[];
 }
 
-let user: User = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-    isActive: true,
-    role: "admin",
-    createdAt: new Date(),
-    products: [
-        {
-            id: 1, 
-            name: "Product 1",
-            price: 20
-        },
-        {
-            id: 1, 
-            name: "Product 1",
-        }
-    ]
-}
+const user: User = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isActive: true,
+  role: "admin",
+  createdAt: new Date(),
+  products: [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 20,
+    },
+    {
+      id: 1,
+      name: "Product 1",
+    },
+  ],
+};
+
+console.log(user);
 
 const fullName = (user: User): string => {
-    return `${user.firstName} ${user.lastName}`;
-}
+  return `${user.firstName} ${user.lastName}`;
+};
+
+fullName(user);
 
 const fullNameBis = (fistName: string, lastName: string): void => {
-    console.log(`${fistName} ${lastName}`);
-}
+  console.log(`${fistName} ${lastName}`);
+};
 
-interface IProps {
-    lastName: string;
-    firstName: string;
-}
+fullNameBis("John", "Doe");
+
+// interface IProps {
+//   lastName: string;
+//   firstName: string;
+// }
 
 // const Index = ({lastName, firstName}:IProps) => {
 //     return (
